7bced27b25d1d6f542e46d65d37bd7c4793a0781
mygguy613@gmail.com <mygguy613@gmail.com>
2023-05-02 18:59:31 -0400

    made it a proper table. added a simple addeventlistener

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log('Hello World!');
+function updateNumber() {
+    document.getElementById("amount").innerHTML = 5;
+}
+
+const keypad = document.getElementById("button1");
+keypad.addEventListener("click", updateNumber);
