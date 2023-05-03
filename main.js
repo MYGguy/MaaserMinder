cf9696f1149ef3213e6d4eb4eb16a2ffd9c5a255
mygguy613@gmail.com <mygguy613@gmail.com>
2023-05-03 12:06:25 -0400

    input field works

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
-function updateNumber() {
+/*function updateNumber() {
     document.getElementById("amount").innerHTML = 5;
 }
+*/
 
-const keypad = document.getElementById("button1");
-keypad.addEventListener("click", updateNumber);
+//const keypad = document.getElementById("button1");
+
+document.addEventListener("DOMContentLoaded", () => {
+    const amount = document.getElementById("amount");
+    const keys = document.querySelectorAll(".key")
+    
+    amount.value = "$";
+    
+    keys.forEach(key => {
+    key.addEventListener("click", () => {
+        const value = key.getAttribute("data-value");
+        amount.value += value;
+    })
+})
+})
+
+
+//keypad.addEventListener("click", updateNumber);
