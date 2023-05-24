0ca9a273d3d3f7d03b27207c92b01cd71b652cab
mygguy613@gmail.com <mygguy613@gmail.com>
2023-05-24 12:29:03 -0400

    js declarations

---
+++ New/main2.js
@@ -0,0 +1,14 @@
+//declarations
+const total = document.getElementById('total');
+const inputField = document.getElementById('input-field');
+
+const minusButton = document.getElementById('minus-button');
+const plusButton = document.getElementById('plus-button');
+
+const keys = document.querySelectorAll('.key');
+const backspace = document.getElementById('backspace');
+
+const resetButton = document.getElementById('reset-button');
+
+const minusHistory = document.getElementById('minus-history');
+const plusHistory = document.getElementById('plus-history');
