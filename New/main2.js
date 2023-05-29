d48532cdfb6f08eaab95e2aa933f6115b5326e2b
mygguy613@gmail.com <mygguy613@gmail.com>
2023-05-29 12:43:07 -0400

    percent time activated

--- New/main2.js
+++ New/main2.js
@@ -1,5 +1,5 @@
 //dom declarations
-let total, inputField, minusButton, plusButton, percentButton, keys, backspace, resetButton, minusHistory, plusHistory;
+let total, inputField, percentField, minusButton, plusButton, percentButton, keys, backspace, resetButton, minusHistory, plusHistory;
 
 //lets declarations
 let currentNumber = [];
@@ -7,10 +7,14 @@ let plusHistoryNumbers = [];
 let minusHistoryNumbers = [];
 let runningTotal = 0;
 
+let percentTime;
+let percentNumber = [];
+
 //when page starts, assign variables
 document.addEventListener("DOMContentLoaded", () => {
     total = document.getElementById('total');
     inputField = document.getElementById('input-field');
+    percentField = document.getElementById('percent-field')
 
     minusButton = document.getElementById('minus-button');
     plusButton = document.getElementById('plus-button');
@@ -34,32 +38,50 @@ function keyPress() {
 //get key
             let keyValue = key.getAttribute("data-value");
 //update current number
+			if (percentTime === false) {
             currentNumber += keyValue;
 //update input field
             inputField.value = "$" + currentNumber;
+            
+			} else {
+				percentNumber += keyValue;
+				percentField.value = percentNumber + "%";
+			}
         });
     });
 };
 
 //update total and histories when submitted
 function submitButton(plusOrMinus, plusOrMinusNumbers, plusOrMinusHistory) {
-//add to history numbers array
-plusOrMinusNumbers.unshift(currentNumber);
-
-let ol = document.createElement("ol");
-plusOrMinusHistory.innerHTML = '';
-
-//write each number into the history
-plusOrMinusNumbers.forEach(number => {
-	let li = document.createElement("li");
-	li.textContent = "$" + number;
-	ol.appendChild(li);
+	if (percentTime === true) {
+		console.log('hello');
+	}
+		
+	//add to history numbers array
+	plusOrMinusNumbers.unshift(currentNumber);
 	
-})
-plusOrMinusHistory.appendChild(ol);
+	let ol = document.createElement("ol");
+	plusOrMinusHistory.innerHTML = '';
+	
+	//write each number into the history
+	plusOrMinusNumbers.forEach(number => {
+		let li = document.createElement("li");
+		li.textContent = "$" + number;
+		ol.appendChild(li);
+		
+	})
+	plusOrMinusHistory.appendChild(ol);
+	
+	//reset current number and input field
+		currentNumber = [];
+		inputField.value = "$";
+	
+	}
 
-//reset current number and input field
-	currentNumber = [];
-	inputField.value = "$";
-
+function percentFunction() {
+	percentTime = true;
+	percentField.classList.toggle("active");
+	console.log(percentField.classList);
+	
+	console.log("you pressed percent");
 }
